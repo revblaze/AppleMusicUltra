8dd8d483f262f6a86d278f85e1f227bbd7f05cc6
Justin Bush <revblaze@users.noreply.github.com>
2020-08-31 12:08:46 -0400

    Distribute WebView Observers to separate managing files
    Moved WKWebView Observers from ObserverDidChange.swift to ["URL", "Title" and "Progress"] + DidChange.swift
    
    Added call to urlDidChange that passes the current URL to BackNavManger (BackButton.swift) and updates its display, depending on the specified page.

--- Ultra/Ultra/CoreController/Observers/URLDidChange.swift
+++ Ultra/Ultra/CoreController/Observers/URLDidChange.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+// MARK: URL Did Change
+
+extension ViewController {
+    
+    /// Fired when the raw URL webView value changes
+    func urlDidChange(_ urlString: String) {
+        let url = Clean.url(urlString)
+        Debug.log("URL: \(url)")        // Debug: Print URL to Load
+        
+        runLoginCheck()                 // Run Looping Login Check
+        updateBackButtonDisplay(url)    // Toggle Back Button
+        
+        /*
+        if !User.isSignedIn {
+            runLoginStatusCheck()
+        } else {
+            runDisabledPlayerCheck()
+        }
+        */
+    }
+    
+}
