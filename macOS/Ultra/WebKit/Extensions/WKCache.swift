9a5c8b5bcc5bd89eab980217b41b36eb3c0024b7
Justin Bush <revblaze@users.noreply.github.com>
2021-06-10 12:09:00 -0400

    Add WebKit extensions

---
+++ macOS/Ultra/WebKit/Extensions/WKCache.swift
@@ -0,0 +1,30 @@
+//
+//  WKCache.swift
+//  Ultra
+//
+//  Created by Justin Bush on 2021-06-10.
+//
+
+import Foundation
+import WebKit
+
+extension WKWebView {
+    
+    func clearCache() {
+        let websiteDataTypes = NSSet(array: [WKWebsiteDataTypeDiskCache, WKWebsiteDataTypeMemoryCache])
+        let date = Date(timeIntervalSince1970: 0)
+        WKWebsiteDataStore.default().removeData(ofTypes: websiteDataTypes as! Set<String>, modifiedSince: date, completionHandler:{ })
+    }
+    
+}
+
+
+extension ViewController {
+    
+    func clearCache() {
+        let websiteDataTypes = NSSet(array: [WKWebsiteDataTypeDiskCache, WKWebsiteDataTypeMemoryCache])
+        let date = Date(timeIntervalSince1970: 0)
+        WKWebsiteDataStore.default().removeData(ofTypes: websiteDataTypes as! Set<String>, modifiedSince: date, completionHandler:{ })
+    }
+    
+}
---
+++ macOS/Ultra/WebKit/Extensions/WKExtensions.swift
@@ -0,0 +1,51 @@
+//
+//  WKExtensions.swift
+//  Ultra
+//
+//  Created by Justin Bush on 2021-06-10.
+//
+
+import Foundation
+import WebKit
+
+// MARK: WKWebView
+extension WKWebView {
+    /// Quick and short load URL String in a WKWebView
+    func load(_ string: String) {
+        if let url = URL(string: string) {
+            let request = URLRequest(url: url)
+            load(request)
+        }
+    }
+    /// Quick and short load URL in a WKWebView
+    func load(_ url: URL) {
+        let request = URLRequest(url: url)
+        load(request)
+    }
+    /// Quick load a `file` (without `.html`) and `path` to the directory
+    /// # Usage
+    ///     webView.loadFile("index", path: "Website")
+    ///  - parameters:
+    ///     - name: Name of the HTML file to load (without `.html`, ie. `"index"`)
+    ///     - path: Path where the HTML file is located (`"website"` for `website/index.html`)
+    func load(file: String, path: String) {
+        if let url = Bundle.main.url(forResource: file, withExtension: "html", subdirectory: path) {
+            self.loadFileURL(url, allowingReadAccessTo: url)
+            let request = URLRequest(url: url)
+            load(request)
+        }
+    }
+    /// Quick load a `file` (without `.html`) and `path` to the directory
+    /// # Usage
+    ///     webView.loadFile("index", path: "Website")
+    ///  - parameters:
+    ///     - name: Name of the HTML file to load (without `.html`, ie. `"index"`)
+    ///     - path: Path where the HTML file is located (`"website"` for `website/index.html`)
+    func loadFile(_ name: String, path: String) {
+        if let url = Bundle.main.url(forResource: name, withExtension: "html", subdirectory: path) {
+            self.loadFileURL(url, allowingReadAccessTo: url)
+            let request = URLRequest(url: url)
+            load(request)
+        }
+    }
+}
