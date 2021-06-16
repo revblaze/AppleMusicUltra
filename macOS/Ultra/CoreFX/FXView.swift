f063be2e8b3e8f0c382373b33b08d0cdc534fe26
Justin Bush <revblaze@users.noreply.github.com>
2021-06-16 09:05:34 -0400

    Import Legacy Styles from Ultra-alpha.1

--- macOS/Ultra/CoreFX/FXView.swift
+++ macOS/Ultra/CoreFX/FXView.swift
@@ -11,6 +11,7 @@ class FXView: NSView {
 
     @IBOutlet weak var blurView: NSVisualEffectView!
     @IBOutlet weak var imageView: NSImageView!
+    @IBOutlet weak var backgroundView: NSImageView!
     
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
@@ -18,4 +19,16 @@ class FXView: NSView {
         // Drawing code here.
     }
     
+}
+
+
+
+enum LegacyStyles {
+    case preset     // Preset       (Default)                   0           .appearanceBased
+    case frosty     // Frosty       (Light Opaque)              11          .sheet
+    case bright     // Bright       (Light Middle)              8           .mediumLight
+    case energy     // Energy       (Light Transparent)         1           .light
+    case cloudy     // Cloudy       (Dark Opaque)               9           .ultraDark
+    case shadow     // Shadow       (Dark Middle)               17          .toolTip
+    case vibing     // Vibing       (Dark Transparent)          2           .dark
 }
